29ad8b2fae8af8f211a88869175d56304ab3b295
AlphaData01 <ferchoarezaragoza@gmail.com>
2024-11-24 23:09:01 -0600

    Segundo Commit

--- main/main.c
+++ main/main.c
@@ -2,10 +2,13 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
 #define led 2
 
 uint8_t led_level = 0;
+static const char* tag = "Main";
+uint8_t count = 0;
 
 esp_err_t init_led(void);
 esp_err_t blink_led(void);
@@ -18,8 +21,20 @@ void app_main(void)
 
         vTaskDelay(200/portTICK_PERIOD_MS);
         blink_led();
-        printf("Led level: %u\n", led_level);
         
+        count += 1;
+        if(count > 30){
+            count=0;
+        }
+        if(count < 10){
+            ESP_LOGI(tag, "Value: %u.", count);
+        }
+        if(count >= 10 && count < 20){
+            ESP_LOGW(tag, "Value: %u.", count);
+        }
+        if(count >= 20){
+            ESP_LOGE(tag, "Value: %u.", count);
+        }
     }
 }
 
